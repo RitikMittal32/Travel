bd15bc2e266312c5d082c7a02d4437555d915d1f
RitikMittal32 <ritikmittal8068@gmail.com>
2023-07-05 11:19:07 +0530

    third commit

--- tarvel.js
+++ tarvel.js
@@ -23,3 +23,26 @@ menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   sidebar.classList.toggle('expand');
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const plane = document.getElementById('plane');
+  const planeContainer = document.getElementById('plane-container');
+  let containerWidth;
+  let position = -100;
+  let direction = 1;
+
+  function animatePlane() {
+    containerWidth = planeContainer.offsetWidth;
+
+    position += 3 * direction;
+    
+    if (position > containerWidth) {
+      position = -100;
+    }
+    plane.style.left = position + 'px';
+
+    requestAnimationFrame(animatePlane);
+  }
+  animatePlane();
+});
